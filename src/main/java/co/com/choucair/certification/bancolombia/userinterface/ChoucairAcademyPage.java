ea211e2c2a66f494a1620a6d7c51dce6bf1164e3
Jpinzon07 <70151797+Jpinzon07@users.noreply.github.com>
2021-02-20 13:08:41 -0500

    Actualizacion 20 de Febrero 2021
    Pruebas Choucair Testing
    **Se crea un repositorio en GitHub para cargar las pruebas descritas en el LMS de Choucair Testing

--- src/main/java/co/com/choucair/certification/bancolombia/userinterface/ChoucairAcademyPage.java
+++
@@ -1,8 +0,0 @@
-package co.com.choucair.certification.bancolombia.userinterface;
-
-import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("https://operacion.Choucairtesting.com/academy/login/index.php")
-public class ChoucairAcademyPage extends PageObject {
-}
